b63728386015434f3400e25b13e1c6a8cf520776
Roman Permiakov <info@presstomsk.ru>
2023-02-25 16:58:32 +0700

    Add menu

--- src/main/java/com/example/ChatBotElise/service/TelegramBot.java
+++ src/main/java/com/example/ChatBotElise/service/TelegramBot.java
@@ -3,26 +3,36 @@ package com.example.ChatBotElise.service;
 import com.example.ChatBotElise.config.BotConfig;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot{
 
-    final BotConfig config;
+    private static final String HELP_TEXT = "This bot created to demonstrate Spring\n\n" +
+                                             "You can execute commands from main menu!\n\n" +                                             
+                                             "Type /start to see a welcome message\n\n" +
+                                             "Type /mydata to see data stored about yourself\n\n" +
+                                             "Type /help to see this message again";
+	final BotConfig config;    
     private static Logger logger = Logger.getLogger(TelegramBot.class.getName());
     private static FileHandler handler; 
 
     public TelegramBot(BotConfig config) {
-        this.config = config;
+        this.config = config;       
         try {
 			this.handler = new FileHandler("logs.json");
 			logger.addHandler(handler);		
@@ -31,6 +41,19 @@ public class TelegramBot extends TelegramLongPollingBot{
 		} catch (IOException e) {				
 			logger.log(Level.SEVERE,e.getMessage());
 		}
+        
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info about this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+        	this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch(TelegramApiException ex) {
+        	logger.log(Level.SEVERE,ex.getMessage());
+        }
+        
     }
 
     @Override
@@ -52,12 +75,17 @@ public class TelegramBot extends TelegramLongPollingBot{
 
             switch (messageText) {
                 case "/start":
-                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());	
-                        logger.info("Pressed start by " + update.getMessage().getChat().getFirstName());
-                        break;
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());	
+                    logger.info("Pressed start by " + update.getMessage().getChat().getFirstName());
+                    break;
+                
+                case "/help":
+                	sendMessage(chatId, HELP_TEXT); 
+                	logger.info("Pressed help by " + update.getMessage().getChat().getFirstName());   
+                	break;
                 default:
 
-                        sendMessage(chatId, "Sorry, command was not recognized");
+                    sendMessage(chatId, "Sorry, command was not recognized");
 
             }
         }
